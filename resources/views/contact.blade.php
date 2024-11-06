e07993e0a1bd399d962b441e98ae7936abed042b
MarvinCornejo17 <marvinjaviercornejolopez@gmail.com>
2024-11-06 10:12:32 -0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/forms.css"> <!-- Cambia el path según tu estructura -->
+    <link rel="stylesheet" href="/css/forms.css"> <!-- Cambia el path según tu estructura -->
 </head>
 <body class="bg-dark text-light">
 
