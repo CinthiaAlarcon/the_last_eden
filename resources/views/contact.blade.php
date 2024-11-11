3307eb1d2f8ca33f68c87baed384f419c7242738
MarvinCornejo17 <marvinjaviercornejolopez@gmail.com>
2024-11-11 16:39:39 -0700

    icon2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/css/forms.css"> <!-- Cambia el path según tu estructura -->
-    <link rel="icon" href="imagenes/icons/icon-16x16.png" type="image/png">
+    <link rel="icon" href="imagenes/icon-16x16.png" type="image/png">
 
 </head>
 <body class="bg-dark text-light">
